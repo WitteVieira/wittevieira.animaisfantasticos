758418807ea66a7e602958bbe14382fc6dc84abd
WitteVieira <witte.vieira.carvalho@gmail.com>
2023-01-29 18:33:34 -0300

    adicionado package-lock

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,6 +1,8 @@
 export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  const activeClass = "ativo";
 
   function activeAccordion() {
     this.classList.toggle(activeClass);
@@ -12,7 +14,7 @@ export default function initAccordion() {
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
     accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+      item.addEventListener("click", activeAccordion);
     });
   }
 }
