612e8cf2cebd42bc3dcf0d5173469d3db0d30369
WitteVieira <witte.vieira.carvalho@gmail.com>
2023-02-15 15:55:24 -0300

    resfatorar-anima-numeros

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from './anima-numeros.js';
+import AnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
@@ -17,7 +17,10 @@ export default function initFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
+
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      console.log(animaNumeros);
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
