fdcf43953176a9521e1f4e73bf03d4e312f040b0
WitteVieira <witte.vieira.carvalho@gmail.com>
2023-02-17 19:52:46 -0300

    anima-numeros refatorado

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -2,13 +2,14 @@ export default class AnimaNumeros {
   constructor(numeros, observerTarget, observerClass) {
     this.numeros = document.querySelectorAll(numeros);
     this.observerTarget = document.querySelector(observerTarget);
-    this.observerClasss = observerClass;
+    this.observerClass = observerClass;
 
+    // bind o this do objeto ao callback da mutação
     this.handleMutation = this.handleMutation.bind(this);
   }
 
-  // Recebe um elemento do dom, com o número em seu texto
-  // Incrementa a partir de 0 até o número final
+  // Recebe um elemento do dom, com número em seu texto
+  // incrementa a partir de 0 até o número final
   static incrementarNumero(numero) {
     const total = +numero.innerText;
     const incremento = Math.floor(total / 100);
@@ -26,11 +27,10 @@ export default class AnimaNumeros {
   // Ativa incrementar número para cada
   // número selecionado do dom
   animaNumeros() {
-    this.numeros.forEach((numero) => {
-      this.constructor.incrementarNumero(numero);
-    });
+    this.numeros.forEach(numero => this.constructor.incrementarNumero(numero));
   }
 
+  // Função que ocorre quando a mutações ocorrer
   handleMutation(mutation) {
     if (mutation[0].target.classList.contains(this.observerClass)) {
       this.observer.disconnect();
@@ -38,6 +38,8 @@ export default class AnimaNumeros {
     }
   }
 
+  // Adiciona o MutationObserver para verificar
+  // quanto a classe ativo é adiciona ao element target
   addMutationObserver() {
     this.observer = new MutationObserver(this.handleMutation);
     this.observer.observe(this.observerTarget, { attributes: true });
